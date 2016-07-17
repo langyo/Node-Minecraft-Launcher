7b7cf8715a88a36a5a22bfc74919a592259b3a69
ncbql <764798966@qq.com>
2016-07-17 13:24:27 +0800

    更改文件目录

---
+++ main.js
@@ -0,0 +1,4 @@
+module.exports.LauncherCore=require('./lib/core/LauncherCore.js');
+module.exports.SystemTools=require('./lib/tools/SystemTools.js');
+module.exports.OfflineAuthenticator=require('./lib/authentication/OfflineAuthenticator.js');
+module.exports.Yggdrasil=require('./lib/authentication/Yggdrasil.js');
