8249671314af80d2d8273489bd044249e27dce6b
ncbql <764798966@qq.com>
2016-07-26 22:33:21 +0800

    没提交上去

---
+++ test.js
@@ -0,0 +1,7 @@
+const s=require('./lib/tools/SystemTools.js');
+log('当前系统: '+s.getSystemType());
+log('当前系统位数: '+s.GetArch());
+console.log('\u001b[33m[minecraft-launcher]\u001b[32m %s\u001b[0m','已安装成功！');
+function log(msg){
+    console.log('\u001b[33m[minecraft-launcher]\u001b[36m %s\u001b[0m',msg);
+}
